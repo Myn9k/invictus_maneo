4370c0d058b23ee9ec5fb1e4862110fcda974577
Myn9k <rodionpolyakov123@gmail.com>
2024-05-15 17:39:41 +0900

    Почти готовый проект от Invictus Maneo

---
+++ invictusmaneo/main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Data)
+admin.site.register(Files)
+admin.site.register(Questions)
+admin.site.register(Images)
+
+# Register your models here.
